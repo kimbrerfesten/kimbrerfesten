6f472782a09370870d8255eb4b6455c8487d89f1
ne.linnemann@gmail.com <ne.linnemann@gmail.com>
2017-02-07 15:42:51 +0100

    Add fonts and some design

---
+++ static/js/init.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+      $('.dropdown-button').dropdown({
+                inDuration: 300,
+                outDuration: 225,
+                hover: true,
+                belowOrigin: true,
+                alignment: 'right'
+            });
+            // Initialize collapse button
+        $(".button-collapse").sideNav();
+});
