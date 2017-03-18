114d93243bb8649fd37400ccf046247023d6b884
Linnemann <ne.linnemann@gmail.com>
2017-03-18 12:40:14 +0100

    Make changes from meeting

--- static/js/init.js
+++ static/js/init.js
@@ -1,5 +1,6 @@
  $(document).ready(function(){
     $(".button-collapse").sideNav({
+        menuWidth: 200, // Default is 300
         'edge': 'left',
         closeOnClick: false, // Closes side-nav on &lt;a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
